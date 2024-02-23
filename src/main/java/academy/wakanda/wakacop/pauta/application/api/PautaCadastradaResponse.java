8f915bf0403c3c38afd7ca81053cd5a40dcf7db3
ranylsonxbr <ranylson1212@gmail.com>
2024-02-23 07:01:07 -0300

    implementa camada api e flow para service

---
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
@@ -0,0 +1,10 @@
+package academy.wakanda.wakacop.pauta.application.api;
+
+import lombok.Builder;
+
+import java.util.UUID;
+
+@Builder
+public class PautaCadastradaResponse {
+    private UUID id;
+}
